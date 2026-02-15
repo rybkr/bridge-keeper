0cda4b66aed4f16da83f3919f4ebd445318aceb7
Joel <joelshaw577@gmail.com>
2026-02-15 15:45:42 -0500

    Initial Template for Gemini API

--- cmd/bridgekeeper/main.go
+++ cmd/bridgekeeper/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/genai"
 )
 
+const model = "gemini-2.5-flash-lite"
+
 func main() {
-	fmt.Println("bridgekeeper")
+	ctx := context.Background()
+	var err error
+	err = godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey: os.Getenv("GEMINI_API_KEY"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := client.Models.GenerateContent(
+		ctx,
+		model,
+		genai.Text("Explain how AI works in a few words"),
+		nil,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(result.Text())
 }
